ffd05c9d0bd752c0e23c8a5040f20bc92a3fda2d
webzunft <webzunft@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-08 19:59:46 +0000

    version 1.2.7
    git-svn-id: https://plugins.svn.wordpress.org/blockalyzer-adblock-counter/trunk@835117 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- blockalyzer.php
+++ blockalyzer.php
@@ -1,7 +1,7 @@
 <?php
 /*
   Plugin Name: BlockAlyzer - Adblock counter
-  Version: 1.2.6.1
+  Version: 1.2.7
   Plugin URI: http://webgilde.com/en/blockalyzer/
   Description: Count how many of your visitors are using an adblock plugin.
   Author: Thomas Maier
@@ -33,7 +33,7 @@ if (!function_exists('add_action')) {
     exit();
 }
 
-define('BAVERSION', '1.2.6.1');
+define('BAVERSION', '1.2.7');
 define('BANAME', 'blockalyzer-adblock-counter');
 define('BATD', 'blockalyzer');
 define('BADIR', basename(dirname(__FILE__)));
@@ -372,7 +372,7 @@ if (!class_exists('BA_CLASS')) {
                 </div><!-- .postbox -->
                 <div class="postbox isc-setting-group">
                 <h3 class="setting-group-head"><?php _e('Benchmark', BATD) ?></h3>
-                <table class="form-table"><tbody><tr><td>
+                <table class="form-table"><tbody>
                 <?php
             }
 
